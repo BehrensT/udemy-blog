b643c8ca62f0613b95a1ec715578257d891bc6c9
Tom <BehrensT@gmail.com>
2023-01-28 22:33:31 -0500

    events and events

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,8 +5,13 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
     const event = req.body;
+
+    events.push(event);
+
 
     axios.post("http://localhost:4000/events", event).catch((err) => {
         console.log(err.message);
@@ -33,6 +38,11 @@ app.post("/events", (req, res) => {
     });
 
     res.send({ status: "OK" });
+});
+
+
+app.get('/events', (req, res) => {
+    res.send(events)
 });
 
 app.listen(4005, () => {
